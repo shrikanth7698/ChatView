c0bef91a0de96f7e5f3f1fab6b94c400ca5965e4
shrikanth7698 <shrikanth7698@gmail.com>
2018-02-16 16:00:13 +0530

    Added required  resources

--- app/src/main/java/com/shrikanthravi/chatviewlibrary/ChatActivity.java
+++ app/src/main/java/com/shrikanthravi/chatviewlibrary/ChatActivity.java
@@ -3,11 +3,11 @@ package com.shrikanthravi.chatviewlibrary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ChatActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_chat);
     }
 }
