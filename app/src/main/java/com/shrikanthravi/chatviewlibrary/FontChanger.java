c1f80bea12cb16da82b9d14ab1e6bf76b04a29b6
shrikanth7698 <shrikanth7698@gmail.com>
2018-02-16 16:01:43 +0530

    Added universal font changer

---
+++ app/src/main/java/com/shrikanthravi/chatviewlibrary/FontChanger.java
@@ -0,0 +1,31 @@
+package com.shrikanthravi.chatviewlibrary;
+
+import android.graphics.Typeface;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by shrikanthravi on 16/02/18.
+ */
+
+public class FontChanger {
+    private Typeface typeface;
+
+    public FontChanger(Typeface typeface){
+        this.typeface = typeface;
+    }
+
+    public void replaceFonts(ViewGroup viewTree){
+        View child;
+        for(int i=0;i<viewTree.getChildCount();++i){
+            child = viewTree.getChildAt(i);
+            if(child instanceof ViewGroup){
+                replaceFonts((ViewGroup) child);
+            }
+            else if(child instanceof TextView){
+                ((TextView) child).setTypeface(typeface);
+            }
+        }
+    }
+}
